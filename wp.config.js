98830416194907759572545aa3f14c87446bc4f2
JoseGon20335 <100246655+JoseGon20335@users.noreply.github.com>
2022-03-03 21:49:21 -0600

    Add files via upload

--- wp.config.js
+++ wp.config.js
@@ -5,5 +5,13 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+  },
+module: {
+    test: /\.scss$/,
+    use: [
+        "style-loader", 
+        "css-loader", 
+        "sass-loader" 
+    ]
   }
 };
