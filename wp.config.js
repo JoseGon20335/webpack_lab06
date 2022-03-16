9eb7947183dbec44e159c114e6ecd81edbd649cc
JoseGon20335 <100246655+JoseGon20335@users.noreply.github.com>
2022-03-15 23:44:02 -0600

    BabelImplements
    Se implemento babel.
    Ademas que ahora se puede correr unicamente con el ' *npm run start* '

--- wp.config.js
+++ wp.config.js
@@ -2,16 +2,26 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist")
-  },
-module: {
+
+  module: {
+    rules:[
+      {
+        test: /\.js$/,
+        exclude:/node_modules/,
+        use:["babel-loader"],
+      }
+    ],
+
     test: /\.scss$/,
     use: [
         "style-loader", 
         "css-loader", 
         "sass-loader" 
     ]
+  },
+
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
   }
 };
